2c60ee5eab9ffbeb25f54ea4f7b9368a8dc6bb4d
andy <1978930262@qq.com>
2022-04-23 23:03:49 +0800

    使用多个组件.a静态库

---
+++ cmake_example/use_multi_components_lib/components/second_component/include/second_component.h
@@ -0,0 +1,8 @@
+#ifndef _SECOND_COMPONENT_H_
+#define _SECOND_COMPONENT_H_
+
+
+void printf_second_component();
+
+
+#endif
---
+++ cmake_example/use_multi_components_lib/components/third_component/include/third_component.h
@@ -0,0 +1,8 @@
+#ifndef _THIRD_COMPONENT_H_
+#define _THIRD_COMPONENT_H_
+
+
+void printf_third_component();
+
+
+#endif
